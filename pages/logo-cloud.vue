7802c7f5ecf5a7adc01184975631cda3c54c0533
andyydao <anddy@Andys-MacBook-Pro.local>
2022-10-01 11:29:19 +0100

    Logo clodu done

--- pages/logo-cloud.vue
+++ pages/logo-cloud.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Standard Logo Cloud -->
-    <section class="py-20">
+    <section class="py-12">
       <div
         class="px-6 xl:px-0 mx-auto flex flex-row flex-wrap max-w-[1186px] items-center justify-center"
       >
@@ -34,15 +34,13 @@
     </section>
 
     <!-- Standard Logo with Text -->
-    <section class="py-20">
+    <section class="py-12">
       <div
         class="px-6 xl:px-0 mx-auto flex flex-col max-w-[1186px] justify-center"
       >
-        <p
-          class="text-[#160042] text-xl font-bold mb-8 text-center xl:text-left"
-        >
+        <h2 class="text-black text-xl font-bold mb-8 text-center xl:text-left">
           Here's some of the awesome companies that use Devjoy
-        </p>
+        </h2>
         <div class="flex flex-row flex-wrap justify-center xl:justify-between">
           <img
             class="h-10 mr-5 my-5"
@@ -70,6 +68,131 @@
             alt=""
           />
         </div>
+      </div>
+    </section>
+
+    <!-- Logo Grid 2x3 -->
+    <section class="py-12">
+      <div
+        class="px-6 xl:px-0 mx-auto flex flex-col max-w-[1186px] justify-center"
+      >
+        <p class="text-black text-xl font-bold mb-8 text-center lg:text-center">
+          Here's some of the awesome companies that use Devjoy
+        </p>
+        <div
+          class="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-center mx-auto bg-[#F2F2F2] rounded-md py-5 px-4 lg:px-16 lg:py-10"
+        >
+          <img
+            class="h-10 mx-5 lg:mx-10 xl:mx-14 my-5 lg:my-8"
+            src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FMicrosoft%20Logo%20(1).svg?alt=media&token=00b75235-7add-4e95-b503-328d9ce4f353"
+            alt=""
+          />
+          <img
+            class="h-10 mx-5 lg:mx-10 xl:mx-14 my-5 lg:my-8"
+            src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FPayPal%20Logo.svg?alt=media&token=1fec3c7a-d52a-48b9-b33a-0349cda1a86e"
+            alt=""
+          />
+          <img
+            class="h-10 mx-5 lg:mx-10 xl:mx-14 my-5 lg:my-8"
+            src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FGoogle%20Logo.svg?alt=media&token=b8512f21-5e04-41fd-b156-7b3855ebc0a5"
+            alt=""
+          />
+          <img
+            class="h-10 mx-5 lg:mx-10 xl:mx-14 my-5 lg:my-8"
+            src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FWalmart%20Logo.svg?alt=media&token=abc4f20b-e63e-4ca0-b4f3-871f2c00af8d"
+            alt=""
+          />
+          <img
+            class="h-10 mx-5 lg:mx-10 xl:mx-14 my-5 lg:my-8"
+            src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FChase%20Logo%20(1).svg?alt=media&token=bfa1f353-e8d5-4f67-852d-0de3fbcc94dc"
+            alt=""
+          />
+
+          <img
+            class="h-10 mx-5 lg:mx-12 xl:mx-14 my-5"
+            src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FSlack%20Logo.svg?alt=media&token=d33668c1-ce9a-4c85-aaa7-7d7a1bfddfc8"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+
+    <!-- Logo Hero Grid 3x2 -->
+    <section class="py-12">
+      <div class="w-screen">
+        <header
+          class="w-full px-6 xl:px-0 max-w-[1186px] justify-center items-center mx-auto flex flex-col lg:flex-row lg:justify-between"
+        >
+          <!-- TEXT SECTION -->
+          <div class="max-w-[556px] lg:mr-12">
+            <h1
+              class="text-black text-[32px] leading-[45px] lg:text-[40px] lg:leading-[52px] text-center font-bold lg:text-left"
+            >
+              Use by the world's best companies
+            </h1>
+
+            <p
+              class="font-inter text-base font-light text-center lg:text-left px-5 lg:px-0 leading-[30px] mt-4 text-[#71717A] max-w-[600px] lg:max-w-[500px] mx-auto lg:mx-0"
+            >
+              “Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
+              aliquam, purus sit amet luctus venenatis, lectus magna fringilla
+              urna, porttitor rhoncus dolor purus non enim”
+            </p>
+
+            <div
+              class="flex flex-row flex-wrap justify-center lg:justify-start items-center mt-12"
+            >
+              <button
+                class="px-6 py-4 font-inter text-base text-white bg-black w-full rounded-md max-w-[400px] md:w-auto"
+              >
+                Try it out
+              </button>
+
+              <button
+                class="px-6 py-4 font-inter text-base text-black bg-[#D7D7D7] mt-5 md:mt-0 md:ml-5 w-full rounded-md max-w-[400px] md:w-auto"
+              >
+                Learn More
+              </button>
+            </div>
+          </div>
+
+          <!-- LOGO SECTION -->
+          <div
+            class="flex flex-row flex-wrap mt-12 max-w-[700px] lg:max-w-[550px] xl:max-w-[680px] lg:h-[500px] justify-center items-center mx-auto bg-[#F2F2F2] rounded-md py-12 px-2 lg:px-12 lg:mt-0 lg:justify-between"
+          >
+            <img
+              class="h-10 mx-5 xl:mx-12 my-5"
+              src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FMicrosoft%20Logo%20(1).svg?alt=media&token=00b75235-7add-4e95-b503-328d9ce4f353"
+              alt=""
+            />
+            <img
+              class="h-10 mx-5 xl:mx-12 my-5"
+              src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FPayPal%20Logo.svg?alt=media&token=1fec3c7a-d52a-48b9-b33a-0349cda1a86e"
+              alt=""
+            />
+            <img
+              class="h-10 mx-5 xl:mx-12 my-5"
+              src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FGoogle%20Logo.svg?alt=media&token=b8512f21-5e04-41fd-b156-7b3855ebc0a5"
+              alt=""
+            />
+            <img
+              class="h-10 mx-5 xl:mx-12 my-5"
+              src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FWalmart%20Logo.svg?alt=media&token=abc4f20b-e63e-4ca0-b4f3-871f2c00af8d"
+              alt=""
+            />
+            <img
+              class="h-10 mx-5 xl:mx-12 my-5"
+              src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FChase%20Logo%20(1).svg?alt=media&token=bfa1f353-e8d5-4f67-852d-0de3fbcc94dc"
+              alt=""
+            />
+
+            <img
+              class="h-10 mx-5 xl:mx-12 my-5"
+              src="https://firebasestorage.googleapis.com/v0/b/tailwindfolio.appspot.com/o/templates%2Fassets%2FSlack%20Logo.svg?alt=media&token=d33668c1-ce9a-4c85-aaa7-7d7a1bfddfc8"
+              alt=""
+            />
+          </div>
+        </header>
       </div>
     </section>
   </div>
